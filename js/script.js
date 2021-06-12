d4070759ab785345c37c176f0fab6632c8f7fc64
showrav38 <showravsaha38@gmail.com>
2021-06-12 23:48:49 +0600

    port

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".starter").mouseout(function(event) {
+    $(".starter").css("background-color", "#e74c3c");
+  });
+  $(".premimum").mouseover(function(event) {
+    $(".premimum").css("background-color", "#e74c3c");
+  });
+  $(".business").mouseover(function(event) {
+    $(".business").css("background-color", "#e74c3c");
+  });
+});
